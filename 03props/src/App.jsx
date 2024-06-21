9b85268699cb7eeaef05eb3dc31b78c8a6618e8c
askgurdit <askditman@gmail.com>
2024-06-21 21:26:26 +0530

    component

---
+++ 03props/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import card from './components/card.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-green-400 text-green-900 h-screen w-full flex justify-center items-center'>
+        <card />
+      </div>
+    </>
+  );
+}
+
+export default App;
