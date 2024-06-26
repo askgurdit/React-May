161b9bdbfbd7e9c87b9aba614e4d0a7418afc266
askgurdit <askditman@gmail.com>
2024-06-26 20:02:32 +0530

    Currency converter App version1.0

--- 06CurrencyConverter/src/components/InputBox.jsx
+++ 06CurrencyConverter/src/components/InputBox.jsx
@@ -1,5 +1,5 @@
 // For from container
-import React from 'react';
+import React , {useId} from 'react';
 
 function InputBox({
     label,
@@ -12,13 +12,15 @@ function InputBox({
     currencyDisable = false,
     className = "",
 }) {
+    const amountInputId = useId()
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
