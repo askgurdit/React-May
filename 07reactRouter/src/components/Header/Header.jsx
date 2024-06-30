9c8b54b01e1a0b69a96243bcc19d13eec1a41b56
askgurdit <askditman@gmail.com>
2024-06-30 17:53:06 +0530

    React routing and useLoaderData

--- 07reactRouter/src/components/Header/Header.jsx
+++ 07reactRouter/src/components/Header/Header.jsx
@@ -62,6 +62,26 @@ export default function Header() {
                                     Contact
                                 </NavLink>
                             </li>
+                            <li>
+                                <NavLink
+                                    to="/user/1"
+                                    className={({ isActive }) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? 'text-orange-700' : 'text-gray-700'}`
+                                    }
+                                >
+                                    User
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/github"
+                                    className={({ isActive }) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? 'text-orange-700' : 'text-gray-700'}`
+                                    }
+                                >
+                                    Github
+                                </NavLink>
+                            </li>
                         </ul>
                     </div>
                 </div>
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import './index.css'
-import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {RouterProvider, createBrowserRouter, createRoutesFromElements , Route } from 'react-router-dom'
 import Layout from "./Layout.jsx"
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import Github from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
+import { githubInfoLoader } from './components/Github/Github.jsx'
+
 //import User from './components/User/User.jsx'
 //import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
- const router = createBrowserRouter([
+ /*const router = createBrowserRouter([
    {
      path: '/',
      element: <Layout/>,
@@ -29,23 +33,25 @@ import Contact from './components/Contact/Contact.jsx'
        }
      ]
    }
- ])
+ ]) */
 
-/*const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       />
-    </Route>
+
+
+   const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='user/:userid' element={<User />} />
+        <Route 
+        loader={githubInfoLoader}
+        path='github' 
+        element={<Github />}
+         />
+      </Route>
+    )
   )
-)*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
