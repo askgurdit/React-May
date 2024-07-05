c12cd4b76e53d649229043c0d90e02f49b92181c
askgurdit <askditman@gmail.com>
2024-07-05 10:59:28 +0530

    v1.0 ReduxtoolkitTodo

--- reduxtoolkitTodo/src/App.jsx
+++ reduxtoolkitTodo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
     <h1 className="text-3xl font-bold underline">
       Learn About Redux Toolkit
     </h1>
+    <AddTodo />
+    <Todos />
     </>
   )
 }
